2814db6fc2630efd33be376b4695325e269563c2
jogarces <pixeruuu@gmail.com>
2016-09-03 12:42:42 -1000

    Added Vendors schema.

---
+++ app/lib/collections/Vendors.js
@@ -0,0 +1,46 @@
+/**
+ * Vendors collection.
+ * Initializes Vendors collection and provides methods
+ * for accessing the collection.
+ * */
+
+
+vendors = "Vendors";
+
+Vendors = new Mongo.Collection(vendors);
+
+/**
+ * Schema for Vendors
+ */
+
+Vendors.attachSchema(new SimpleSchema({
+
+  vendorName:{
+    label: "Vendor Name",
+    type: String,
+    optional: false,
+    autoform:{
+      group: vendors,
+      placeholder: "Vendor Name"
+    }
+  },
+
+  products:{
+    label: "Products",
+    type: String,
+    optional: false,
+    allowedValues: function(){
+      return Products.find({}).map(function(object){return object.productName;});
+    },
+    autoform:{
+      group: vendors,
+      placeholder: "Products"
+    }
+  }
+}));
+
+if (Meteor.isServer) {
+  Meteor.publish(vendors, function () {
+    return Vendors.find();
+  });
+}
