76ce4640cd9d5871593bee5e8b98a962dc805811
jogarces <josebeann@gmail.com>
2016-09-12 18:46:42 -1000

    Minor edits

--- app/lib/router/Router.js
+++ app/lib/router/Router.js
@@ -15,10 +15,12 @@ Router.route('/', {
 });
 
 Router.route('/Find-Farmers-Markets',{
+  layoutTemplate: 'GeneralLayout',
   template: 'FarmersMarketMap'
 });
 
 Router.route('/FarmersMarket/:_id', {
+  layoutTemplate: 'GeneralLayout',
   template: 'FarmersMarketProfile',
   data: function() {
     return{fmData: FarmersMarket.find({"_id":this.params._id})};
@@ -26,6 +28,7 @@ Router.route('/FarmersMarket/:_id', {
 });
 
 Router.route('/Vendor/:_id', {
+  layoutTemplate: 'GeneralLayout',
   template: 'VendorsProfile',
   data: function() {
     return{vendorData: Vendors.find({"_id":this.params._id})};
