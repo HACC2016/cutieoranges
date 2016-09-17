8080f7ec9dab1af10d7aa94258673c6715e81ba2
Josephine Garces <jgarces@hawaii.edu>
2016-09-17 08:46:03 -1000

    Formatted date on events

--- app/client/templates/events/Events.js
+++ app/client/templates/events/Events.js
@@ -4,3 +4,9 @@ Template.Events.helpers({
         return EventsFA.find({}).fetch();
     }
 });
+
+UI.registerHelper("localizedDateAndTime", function(date) {
+    if(date) {
+        return moment(date).format('MMMM DD, YYYY'); // shorthand for localized format "5/23/2014 3:47 PM"
+    }
+});
